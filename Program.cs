cd4636a1c837312f4f742d74eea83ee0ded30488
Tim <tim@spreadmaster.net>
2012-11-15 11:26:41 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADSopdr4 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
