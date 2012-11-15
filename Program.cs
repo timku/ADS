883574e34749e39a3f019e159efcd127d1e360b3
Tim <timkuperu+github@gmail.com>
2012-11-15 19:36:23 +0100

    Added code from Lafore

--- Program.cs
+++ Program.cs
@@ -5,7 +5,81 @@ using System.Text;
 namespace ADSopdr4 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello!");
+            int value;
+            Tree theTree = new Tree();
+            theTree.insert(50, 1.5);
+            theTree.insert(25, 1.2);
+            theTree.insert(75, 1.7);
+            theTree.insert(12, 1.5);
+            theTree.insert(37, 1.2);
+            theTree.insert(43, 1.7);
+            theTree.insert(30, 1.5);
+            theTree.insert(33, 1.2);
+            theTree.insert(87, 1.7);
+            theTree.insert(93, 1.5);
+            theTree.insert(97, 1.5);
+            while (true) {
+                Console.Out.Write("Enter first letter of show, ");
+                Console.Out.Write("insert, find, delete, or traverse: ");
+                char choice = getChar();
+                switch (choice) {
+                    case 's':
+                        theTree.displayTree();
+                        break;
+                    case 'i':
+                        Console.Out.Write("Enter value to insert: ");
+                        value = getInt();
+                        theTree.insert(value, value + 0.9);
+                        break;
+                    case 'f':
+                        Console.Out.Write("Enter value to find: ");
+                        value = getInt();
+                        Node found = theTree.find(value);
+                        if (found != null) {
+                            Console.Out.Write("Found: ");
+                            found.displayNode();
+                            Console.Out.Write("\n");
+                        } else {
+                            Console.Out.Write("Could not find ");
+                        }
+                        Console.Out.Write(value + "\n");
+                        break;
+                    case 'd':
+                        Console.Out.Write("Enter value to delete: ");
+                        value = getInt();
+                        bool didDelete = theTree.delete(value);
+                        if (didDelete)
+                            Console.Out.Write("Deleted " + value + "\n");
+                        else
+                            Console.Out.Write("Could not delete ");
+                        Console.Out.Write(value + "\n");
+                        break;
+                    case 't':
+                        Console.Out.Write("Enter type 1, 2 or 3: ");
+                        value = getInt();
+                        theTree.traverse(value);
+                        break;
+                    default:
+                        Console.Out.Write("Invalid entry\n");
+                        break;
+                }  // end switch
+            }  // end while
+        }  // end main()
+        // -------------------------------------------------------------
+        public static String getString() {
+            String s = Console.ReadLine();
+            return s;
         }
-    }
+        // -------------------------------------------------------------
+        public static char getChar() {
+            string s = getString();
+            return s[0];
+        }
+        //-------------------------------------------------------------
+        public static int getInt(){
+            string s = getString();
+            return Int32.Parse(s);
+        }
+        // -------------------------------------------------------------
+    }  // end class TreeApp
 }
---
+++ Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADSopdr4 {
+    class Stack {
+        private int maxSize;
+        private Node[] stackArray;
+        private int top;
+        public Stack()       // constructor
+        {
+            maxSize = 100;
+            stackArray = new Node[maxSize];
+            top = -1;
+        }
+        public void push(Node j)  // put item on top of stack
+        { stackArray[++top] = j; }
+        public Node pop()         // take item from top of stack
+        { return stackArray[top--]; }
+        public Node peek()        // peek at top of stack
+        { return stackArray[top]; }
+        public bool isEmpty()  // true if stack is empty
+        { return (top == -1); }
+        public int size()         // return size
+        { return top + 1; }
+        public Node peekN(int n)  // return item at index n
+        { return stackArray[n]; }
+
+        public void displayStack(String s) {
+            Console.Out.Write(s);
+            Console.Out.Write("Stack (bottom-->top): ");
+            for (int j = 0; j < size(); j++) {
+                Console.Out.Write(peekN(j));
+                Console.Out.Write(" ");
+            }
+            Console.Out.WriteLine("");
+        }
+    }
+}
